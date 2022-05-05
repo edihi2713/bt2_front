833cba263ec720a03f49204d7b05d4beef68028d
Danny Gómez <dmercylove@gmail.com>
2022-05-04 23:40:48 -0500

    creation aforo component

--- src/routes/mainRouter.jsx
+++ src/routes/mainRouter.jsx
@@ -6,12 +6,14 @@ import {
 import Home from "../components/home"
 import Login from "../components/login"
 import Register from "../components/register"
+import Aforo from "../components/Aforo"
 
 const RoutesCollection = (
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/aforo" element={<Aforo />} />
       </Routes>
 );
 
