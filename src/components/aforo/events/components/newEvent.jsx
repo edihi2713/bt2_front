1d55a70f7ea306d69b3262737dabe8dd7541b703
David Leal Pelaez <softwaredevelopertools@gmail.com>
2022-05-26 23:31:46 -0500

    create event form first pass

--- src/components/aforo/events/components/newEvent.jsx
+++ src/components/aforo/events/components/newEvent.jsx
@@ -3,6 +3,10 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Stack from '@mui/material/Stack';
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+
 
 const style = {
   position: 'absolute',
@@ -26,14 +30,61 @@ export default function NewEventModal({open, setOpen}) {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            Crear Evento
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-
-          <Button onClick={() => setOpen(false)}>Cerrar</Button>
-          <Button onClick={() => alert("asdasd")}>Crear</Button>
+          <Box component="form" noValidate sx={{ mt: 3 }}>
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    name="names"
+                    required
+                    fullWidth
+                    id="names"
+                    label="Nombre evento"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    name="email"
+                    type="datetime-local"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Capacidad"
+                    type="number"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={() => alert("asdasd")}
+                >
+                    Guardar
+                </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button
+                    type="button"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={() => setOpen(false)}
+                >
+                    Cerrar
+                </Button>
+                </Grid>
+              </Grid>
+            </Box>
         </Box>
       </Modal>
     </div>
